6a7d2d7c840e6dab7a9e06b4728811369888afb7
waynewbishop <wayne.bishop@arbutusinc.com>
2021-11-10 11:52:30 -0800

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Wayne Bishop on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
